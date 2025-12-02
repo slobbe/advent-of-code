0c0f48d5b33b14f0d002d87495f130d609cbac50
S. Lobbe <slobbe@lobbe.cc>
2025-12-02 17:33:22 +0100

    optimize day1 code

--- 2025/01/main.py
+++ 2025/01/main.py
@@ -1,85 +1,74 @@
 import os
+from typing import List
 
 
-def parse_input(input_path: str):
+def parse_input(input_path: str) -> List[int]:
     path = os.path.join(os.path.dirname(__file__), input_path)
     content = ""
     with open(path) as doc:
         content = doc.read()
 
-    return content.strip().split("\n")
+    content = content.strip()
+    rotations = [int(r.replace("L", "-").replace("R", "")) for r in content.split("\n")]
+    return rotations
 
 
-def parse_rotations(rotation_instructions):
-    rotations = map(lambda r: (r[0], int(r[1:])), rotation_instructions)
-
-    return list(rotations)
-
-
-def execute_rotations(starting_position: int, rotations):
-    dial_max: int = 99
-
-    if starting_position > dial_max:
+def execute_rotations(
+    rotations: List[int], starting_position: int, dial_size: int = 99
+):
+    if starting_position > dial_size:
         return []
 
     positions = [(starting_position, 0)]
     for rotation in rotations:
         current_position = positions[-1][0]
-        direction = rotation[0]
-        distance = rotation[1]
-        if direction == "L":
-            naive_end_position = current_position - distance
-            end_position = naive_end_position % (dial_max + 1)
+        end_position = (current_position + rotation) % (dial_size + 1)
+        zero_crossings_count = abs(rotation) // (dial_size + 1)
 
-            full_rotations = distance // (dial_max + 1)
-            if (end_position > current_position) and (
-                end_position != 0 and current_position != 0
-            ):
-                full_rotations += 1
+        def sign(x):
+            return (x > 0) - (x < 0)
 
-            positions.append((end_position, full_rotations))
-        elif direction == "R":
-            naive_end_position = current_position + distance
-            end_position = naive_end_position % (dial_max + 1)
+        effective_displacement = sign(rotation) * (abs(rotation) % (dial_size + 1))
+        naive_effective_end_position = current_position + effective_displacement
+        is_in_dial_range = (
+            naive_effective_end_position >= 0
+            and naive_effective_end_position <= (dial_size + 1)
+        )
+        if (
+            not is_in_dial_range
+            and effective_displacement != naive_effective_end_position
+        ):
+            zero_crossings_count += 1
 
-            full_rotations = distance // (dial_max + 1)
-            if (end_position < current_position) and (
-                end_position != 0 and current_position != 0
-            ):
-                full_rotations += 1
-
-            positions.append((end_position, full_rotations))
-        else:
-            return []
+        positions.append((end_position, zero_crossings_count))
 
     return positions
 
 
-def get_code(positions):
+def get_passwords(positions):
     end_positions = list(map(lambda p: p[0], positions))
-    full_rotations = list(map(lambda p: p[1], positions))
+    zero_crossings_count = list(map(lambda p: p[1], positions))
 
-    code_1 = end_positions.count(0)
-    code_2 = end_positions.count(0) + sum(full_rotations)
-    return code_1, code_2
+    password_1 = end_positions.count(0)
+    password_2 = password_1 + sum(zero_crossings_count)
+    return password_1, password_2
 
 
 def main():
     input_path: str = "input.txt"
     dial_starting_positions: int = 50
 
-    content = parse_input(input_path)
-    rotations = parse_rotations(content)
-    positions = execute_rotations(dial_starting_positions, rotations)
+    rotations = parse_input(input_path)
+    positions = execute_rotations(rotations, dial_starting_positions)
 
-    code_1, code_2 = get_code(positions)
+    password_1, password_2 = get_passwords(positions)
 
     # Pretty print answers
     print("===== Advent of Code =====")
     print("Day 1: Secret Entrance")
     print("--------------------------")
-    print("Password (Part 1):", code_1)
-    print("Password (Part 2):", code_2)
+    print("Password (Part 1):", password_1)
+    print("Password (Part 2):", password_2)
     print("--------------------------")
 
 
