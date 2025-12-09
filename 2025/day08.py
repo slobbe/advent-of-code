418c22059bb9b8074c6dbc5015d2dad2459c3eb4
S. Lobbe <slobbe@lobbe.cc>
2025-12-09 13:26:43 +0100

    2025-d08: add solution for part 2

--- 2025/day08.py
+++ 2025/day08.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 import numpy as np
 from util import Input, pretty_print, read_input
 
@@ -13,23 +15,40 @@ def distances(nodes: np.ndarray) -> np.ndarray:
 
 
 def get_pairs(
-    distances: np.ndarray, n_connections: int = 1000
+    distances: np.ndarray, n_connections: int | None = None
 ) -> list[tuple[int, int]]:
-    pairs = []
     all_distances = np.unique(distances.flatten())
     sorted_distances = sorted(all_distances[all_distances > 0])
+
+    if n_connections is None:
+        n_connections = len(sorted_distances)
+
+    pairs = deque([])
     for dist in sorted_distances[:n_connections]:
         pair = np.where(distances == dist)[0]
-        pairs.append((float(dist), (int(pair[0]), int(pair[1]))))
+        pairs.appendleft((int(pair[0]), int(pair[1])))
 
-    pairs = [pair for _, pair in sorted(list(set(pairs)))]
+    # pairs = [pair for _, pair in list(set(pairs))]
 
-    return pairs
+    return list(pairs)
 
 
-def connect_circuits(pairs):
+def connect_circuits(jboxes, n_connections: int | None = None, terminate: bool = False):
+    dists = distances(jboxes)
+    all_distances = np.unique(dists.flatten())
+    sorted_distances = sorted(all_distances[all_distances > 0])
+
+    if n_connections is None:
+        n_connections = len(sorted_distances)
+
+    connection_circuits = []
+    last_connection = []
+
     circuits = []
-    for pair in pairs:
+    for i, dist in enumerate(sorted_distances):
+        pair = np.where(dists == dist)[0]
+        pair = (int(pair[0]), int(pair[1]))
+
         if len(circuits) == 0:
             circuits.append({*pair})
             continue
@@ -50,20 +69,28 @@ def connect_circuits(pairs):
 
             circuits.append(set().union(*[*t_circuits, {*pair}]))
 
-    circuits = sorted(circuits, key=len)[::-1]
-    sizes = list(map(len, circuits))
-    return circuits, sizes
+        if (n_connections is not None) and (i == n_connections - 1):
+            connection_circuits = sorted(circuits, key=len)[::-1]
+            if terminate:
+                break
+
+        if len(circuits) == 1 and len(circuits[0]) == len(jboxes):
+            last_connection = [jboxes[pair[0]], jboxes[pair[1]]]
+            break
+
+    sizes = list(map(len, connection_circuits))
+
+    return sizes, last_connection
 
 
 def main() -> None:
-    nodes = get_nodes(read_input(8, False))
-    dist = distances(nodes)
-    pairs = get_pairs(dist)
-    circuits, sizes = connect_circuits(pairs)
+    jboxes = get_nodes(read_input(8, False))
+    sizes, last_connection = connect_circuits(jboxes, 1000, False)
 
     p_1 = eval("*".join([str(s) for s in sizes[:3]]))
+    p_2 = last_connection[0][0] * last_connection[1][0]
 
-    pretty_print([p_1], "Day 8: Playground")
+    pretty_print([p_1, p_2], "Day 8: Playground")
 
 
 if __name__ == "__main__":
